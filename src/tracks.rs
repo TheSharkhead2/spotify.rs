eb8505720cae2f976134d8ed061e13ab3d7269fb
Theo Rode <theorodester@gmail.com>
2022-10-01 09:08:59 -0700

    added get_track_audio_features for getting audio features for a single track

--- src/tracks.rs
+++ src/tracks.rs
@@ -182,6 +182,24 @@ impl Spotify {
         }
     }
 
+    /// Gets audio features for specified track: https://developer.spotify.com/documentation/web-api/reference/#/operations/get-audio-features
+    /// Required scope: none
+    pub fn get_track_audio_features(&self, track_id: &str) -> Result<FeatureTrack, SpotifyError> {
+        let url_extension = format!("audio-features/{}", track_id); // base url
+
+        match self.access_token() { // get access token
+            Ok(access_token) => {
+                match spotify_request(&access_token, &url_extension, RequestMethod::Get) { // make request
+                    Ok(response) => {
+                        return Ok(format_feature_track(&response)) // format and return result
+                    },
+                    Err(e) => return Err(SpotifyError::RequestError(e.to_string())),
+                }
+            },
+            Err(e) => return Err(e), // On error with access token, return error
+        }
+    }
+
     /// Gets audio analysis for specified track: https://developer.spotify.com/documentation/web-api/reference/#/operations/get-audio-analysis
     /// Required scope: none
     pub fn get_track_audio_analysis(&self, track_id: &str) -> Result<AnalysisTrack, SpotifyError> {
