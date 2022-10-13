b50244fcbe6040a3894ee7c2df56188201ec406e
Theo Rode <theorodester@gmail.com>
2022-10-13 13:17:39 -0700

    added some formatting tests

--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,5 @@ mod genres;
 mod markets;
 mod player;
 
-pub use spotify::{Spotify, SpotifyImage, AlbumType, RestrictionReason, ReleaseDatePrecision, ExternalTrackIds, SpotifyContext, SpotifyCollection, Category, Album, DatedAlbum, Artist, Track, DatedTrack, FeatureTrack, Bar, Beat, Section, Segment, Tatum, AnalysisTrack, User, Playlist, PlaylistTrack, TimeRange, RepeatState, Device, PlaybackActions, Playback, SpotifyError}; // re-export relevant structs and enums
+pub use spotify::{Spotify, SpotifyImage, AlbumType, RestrictionReason, ReleaseDatePrecision, ExternalTrackIds, SpotifyContext, SpotifyCollection, Category, Album, DatedAlbum, Artist, Track, DatedTrack, FeatureTrack, Bar, Beat, Section, Segment, Tatum, AnalysisTrack, User, Playlist, PlaylistTrack, TimeRange, RepeatState, Device, PlaybackActions, Playback, SpotifyError, SpotifyObject}; // re-export relevant structs and enums
 
